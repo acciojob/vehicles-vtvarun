c0e480cb0e2c3087ad90e68c10a80ec77b756c19
Varun Tanwar <newvaruntanwar@gmail.com>
2023-12-23 14:43:24 +0530

    Vehicles

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -10,6 +10,11 @@ class Boat implements WaterVehicle{
     int capacity;
 
     String name;
+
+    public Boat(String name, int capacity){
+        this.capacity = capacity;
+        this.name = name;
+    }
     @Override
     public String getVehicleName() {
         return name;
